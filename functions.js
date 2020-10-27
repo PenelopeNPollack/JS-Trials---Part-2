7d89a11838f9d7b39eca18442743749bc7836c84
PenelopeNPollack <PeneloeNPollack@gmail.com>
2020-10-27 21:28:11 +0000

    Implements calculateTotal function
    Co-authored-by: kayteecannon <kaytee.cannon@me.com

--- functions.js
+++ functions.js
@@ -17,4 +17,27 @@ function getFullName(firstName, lastName) {
 
 // 3. calculateTotal
 
-// Define your function here
+function calculateTotal(basePrice, state, tax=0.05) {
+    let subtotal = basePrice * (1 + tax);
+    let fee = 0;
+    
+    if (state == 'CA'){
+        fee = 0.03 * subtotal;
+    }
+
+    else if (state == 'PA') {
+        fee = 2;
+    }
+
+    else if (state == 'MA') {
+        if (base_price <= 100) {
+        fee = 1;
+        }
+    }
+
+    else {
+        fee = 3;
+    }
+    return subtotal + fee;
+
+}
